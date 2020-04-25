3e63b87451cd7318325e6f5fddc3dcaad887e783
Glitch (opaque-momentous-haumea) <none>
2020-04-25 13:48:51 +0000

    🎬💇 Checkpoint ./public/scripts/app.js:12934699/573 ./public/service-worker.js:12934699/115

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -83,9 +83,9 @@ function renderForecast(card, data) {
   const lastUpdated = parseInt(cardLastUpdated);
 
   // If the data on the element is newer, skip the update.
-  if (lastUpdated >= data.currently.time) {
-    return;
-  }
+if (lastUpdated >= data.currently.time) {
+  return;
+}
   cardLastUpdatedElem.textContent = data.currently.time;
 
   // Render the forecast data into the card.
@@ -164,6 +164,21 @@ function getForecastFromNetwork(coords) {
 function getForecastFromCache(coords) {
   // CODELAB: Add code to get weather forecast from the caches object.
 
+  if (!('caches' in window)) {
+  return null;
+}
+const url = `${window.location.origin}/forecast/${coords}`;
+return caches.match(url)
+    .then((response) => {
+      if (response) {
+        return response.json();
+      }
+      return null;
+    })
+    .catch((err) => {
+      console.error('Error getting data from cache', err);
+      return null;
+    });
 }
 
 /**
@@ -198,7 +213,10 @@ function updateData() {
     const location = weatherApp.selectedLocations[key];
     const card = getForecastCard(location);
     // CODELAB: Add code to call getForecastFromCache
-
+getForecastFromCache(location.geo)
+    .then((forecast) => {
+      renderForecast(card, forecast);
+    });
     // Get the forecast data from the network.
     getForecastFromNetwork(location.geo)
         .then((forecast) => {
